30109d77391c887bf400dea5d5b0ce95045f0d57
masaya0521 <kaji0521km@gmail.com>
2022-06-25 22:05:54 +0900

    created

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>{{ text }}</h1>
+    {{ textRef }} <br/>
+    {{ dataReactive }} <br/>
     {{ computed1 }} <br/>
     {{ computed2 }} <br/>
     {{ method("です") }}
@@ -8,21 +10,40 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from '@nuxtjs/composition-api'
+import { computed, defineComponent, ref, reactive } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup() {
-    const text = 'Hello, World!'
+    const text: string = 'Hello, World!'
 
-    const computed1 = computed(() => text + "test")
-    const computed2 = computed(() => text + "2")
+    // data
+    const textRef = ref("ref data") 
+    // data こっちの方がv2のdataに近い？
+    const dataReactive = reactive({
+      form: {
+        name: '',
+      },
+      users: [],
+    })
 
+    // computed
+    const computed1 = computed(() => text + "computed1") 
+    const computed2 = computed(() => text + "computed2")
+
+    // method
     const method = (a: string) => {
-      return "method test" + a
-    }
+      return "method" + a
+    }   
+
+    // createdわからん
+    setTimeout(() => {
+      console.log("created")
+    }, 3000);
 
     return {
       text,
+      textRef,
+      dataReactive,
       computed1,
       computed2,
       method,
