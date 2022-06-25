ec2573192d96d352b507c93e99ad10723f90e259
masaya0521 <kaji0521km@gmail.com>
2022-06-25 21:36:00 +0900

    computedが複数宣言しなければいけないかテスト

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
     <h1>{{ text }}</h1>
+    {{ computed1 }}
+    {{ computed2 }}
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { computed, defineComponent } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup() {
     const text = 'Hello, World!'
 
+    const computed1 = computed(() => text + "test")
+    const computed2 = computed(() => text + "2")
+
     return {
       text,
+      computed1,
+      computed2,
     }
   },
 })
